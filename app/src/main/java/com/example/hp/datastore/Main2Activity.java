7546a874524c2b179df4fcc61a51b1af66bcc5c3
Rajnesh01kaushal <ranjneshkaushal158@gmail.com>
2018-06-23 12:52:55 +0530

    second commit

--- app/src/main/java/com/example/hp/datastore/Main2Activity.java
+++ app/src/main/java/com/example/hp/datastore/Main2Activity.java
@@ -6,27 +6,31 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-    TextView name,age,cls;
-    String userNmae,userAge,userClass;
+    TextView name,email,password,age;
+    String userNmae,userEmail,userPassword,userAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 //        get the refern
-        name = findViewById(R.id.text1);
-        age = findViewById(R.id.text2);
-        cls = findViewById(R.id.text3);
+        name = findViewById(R.id.nameID);
+        email = findViewById(R.id.emailID);
+        password = findViewById(R.id.paswrdID);
+        age = findViewById(R.id.paswrdID);
 
         SharedPreferences sp = getSharedPreferences("details",MODE_PRIVATE);
 
         userNmae = sp.getString("name","none");
+        userEmail = sp.getString("email","none");
+        userPassword = sp.getString("password","none");
         userAge = sp.getString("agr","none");
-        userClass = sp.getString("Class","none");
 
         name.setText(userNmae);
+        email.setText(userEmail);
+        password.setText(userPassword);
         age.setText(userAge);
-        cls.setText(userClass);
+
 
 
     }
